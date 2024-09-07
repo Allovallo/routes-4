faf297e99e8bc4de850f28391ebddb3f0c494cb9
Allovallo <grossco@meta.ua>
2024-09-07 16:50:21 +0300

    files updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { SharedLayout } from "./SharedLayout";
+import { About } from "../pages/About";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/SharedLayout.jsx
@@ -0,0 +1,24 @@
+
+import { Outlet } from "react-router-dom";
+import { Container, Header, Logo, Link } from "./SharedLayout.styled";
+
+export const SharedLayout = () => {
+    return (
+        <Container>
+            <Header>
+                <Logo>
+                    <span role="img" aria-label="computer on">
+                        💻
+                    </span>
+                    GoMerch store
+                </Logo>
+                <nav>
+                    <Link to='/' end>Home</Link>
+                    <Link to='about'>About</Link>
+                    <Link to='products'>Products</Link>
+                </nav>
+            </Header>
+            <Outlet/>
+        </Container>
+    )
+}
