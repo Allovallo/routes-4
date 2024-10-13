81a23257e3302603893f19407bd162951bc175e9
Allovallo <grossco@meta.ua>
2024-10-13 12:17:43 +0300

    files updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { About } from '../pages/About';
+import { Products } from '../pages/Products';
 
 function App() {
   return (
     <Routes>
-      <Route></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/products" element={<Products />}></Route>
     </Routes>
   );
 }
